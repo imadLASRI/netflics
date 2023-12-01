81d4dcd2a4bab787c9d9f0c680b1c2dd17ef4632
LASRi iMAD <imadlasri@hotmail.fr>
2023-12-01 22:24:28 +0100

    fix: removed filter from favorites for now

--- src/views/MyListView.vue
+++ src/views/MyListView.vue
@@ -1,11 +1,10 @@
 <script setup>
 import ItemsList from '../components/ItemsList.vue'
-import FilterItems from '../components/FilterItems.vue'
+
 </script>
 
 <template>
   <h1>MyListView</h1>
-  <FilterItems />
   <ItemsList />
 </template>
 
