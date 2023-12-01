be367610f66a0b143a88252979900f81a2f90d0e
LASRi iMAD <imadlasri@hotmail.fr>
2023-12-01 22:23:57 +0100

    feat: added category filter

--- src/components/FilterItems.vue
+++ src/components/FilterItems.vue
@@ -1,10 +1,35 @@
-<script setup></script>
+<script setup>
+import { defineProps,defineEmits, ref } from 'vue'
+
+defineProps({
+  categories: {
+    type: Object,
+    required: true
+  }
+})
+
+const emit = defineEmits(['categoryChanged'])
+
+const selectedCategory = ref('select')
+
+</script>
 
 <template>
-  <span>filter by "prop filterby: "</span>
-  <select>
-    <option value="">1</option>
-    <option value="">2</option>
-    <option value="">3</option>
-  </select>
+  <div class="flex items-center">
+    <span>Filter by category: </span>
+    <select
+      class="text-black m-5"
+      v-model="selectedCategory"
+      @change="emit('categoryChanged', selectedCategory)"
+    >
+      <option value="select" class="text-gray-400">All categories</option>
+      <option
+        v-for="category in categories"
+        :value="category"
+        :key="category"
+      >
+        {{ category }}
+      </option>
+    </select>
+  </div>
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,47 @@
 <script setup>
+import { onMounted, ref } from 'vue'
+import { cloneDeep } from "lodash"
+
+import { useMovieStore } from '../stores/movieStore'
+
 import ItemsList from '../components/ItemsList.vue'
 import FilterItems from '../components/FilterItems.vue'
+
+const movieStore = useMovieStore()
+let loading = ref(true)
+let movies = ref([])
+let categories = ref([])
+
+function handleCategoryChange(category) {
+  if(category === 'select') {
+    movies.value = cloneDeep(movieStore.movies)
+  } else {
+    movies.value = movieStore.movies.filter(movie => movie.Genre.includes(category))
+  }
+}
+
+onMounted(async () => {
+      try {
+        await movieStore.fetchMovies()
+        movies.value = cloneDeep(movieStore.movies)
+        categories.value = movieStore.moviesCategories
+      } finally {
+        loading.value = false;
+      }
+});
 </script>
 
 <template>
-  <h1>HomeView</h1>
-  <FilterItems />
-  <ItemsList />
+  <span
+    v-if="loading"
+    class="flex justify-center w-full my-10"
+  >
+    Loading...
+  </span>
+  <template v-else>  
+    <FilterItems :categories="categories" @category-changed="handleCategoryChange"/>
+    <ItemsList :movies="movies"/>
+  </template>
 </template>
 
 <style scoped>
