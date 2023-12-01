267452a9ddf25253f68c7eb184aa012e173fd5b9
LASRi iMAD <imadlasri@hotmail.fr>
2023-12-01 22:21:58 +0100

    feat: initiate pinia store

---
+++ src/stores/movieStore.js
@@ -0,0 +1,26 @@
+import { defineStore } from 'pinia'
+
+export const useMovieStore = defineStore('movie', {
+  state: () => ({
+    movies: {}
+  }),
+  getters: {
+    moviesCategories(state) {
+      const allMoviesGenres = state.movies.map((movie) => movie.Genre).join(', ')
+      const distinctMoviesGenres = new Set(allMoviesGenres.split(', '))
+
+      return distinctMoviesGenres
+    }
+  },
+  actions: {
+    async fetchMovies() {
+      try {
+        const response = await fetch('http://localhost:3000/movies')
+        const data = await response.json()
+        this.movies = data
+      } catch (err) {
+        console.error(err)
+      }
+    }
+  }
+})
