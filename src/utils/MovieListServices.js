e3f5eabb4664979ef4e303cfad194624929ff560
LASRi iMAD <imadlasri@hotmail.fr>
2023-12-03 21:50:52 +0100

    feat: creating movie list localstorage manager as a service

---
+++ src/utils/MovieListServices.js
@@ -0,0 +1,43 @@
+export const MovieListServicesManager = (() => {
+  let instance
+
+  const createInstance = () => {
+    let myList = JSON.parse(localStorage.getItem('myList')) || []
+
+    const addToMyList = (movie) => {
+      myList.push(movie)
+      updateLocalStorage()
+    }
+
+    const removeFromMyList = (movieId) => {
+      myList = myList.filter((movie) => movie.id !== movieId)
+      updateLocalStorage()
+    }
+
+    const existsInMyList = (id) => {
+      return !!myList.find((movie) => movie.id === id)
+    }
+
+    const getMyList = () => myList
+
+    const updateLocalStorage = () => {
+      localStorage.setItem('myList', JSON.stringify(myList))
+    }
+
+    return {
+      addToMyList,
+      removeFromMyList,
+      getMyList,
+      existsInMyList
+    }
+  }
+
+  return {
+    getInstance: () => {
+      if (!instance) {
+        instance = createInstance()
+      }
+      return instance
+    }
+  }
+})()
