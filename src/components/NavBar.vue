69b8e9df402f4b72e1e1342d267a9dc1a6043ce2
LASRi iMAD <imadlasri@hotmail.fr>
2023-12-03 05:15:51 +0100

    fix: navbar logo height

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -8,7 +8,7 @@ import SearchBar from './SearchBar.vue'
     <nav class="flex justify-between items-center mx-10 text-white capitalize">
       <div class="flex items-center w-3/6">
         <RouterLink to="/" class="mr-10">
-          <img alt="Netflix logo" class="logo" src="@/assets/logo.svg" width="125" />
+          <img alt="Netflix logo" class="w-60 h-20" src="@/assets/logo.svg" />
         </RouterLink>
         <div class="flex justify-evenly w-2/6">
           <RouterLink to="/">home</RouterLink>
