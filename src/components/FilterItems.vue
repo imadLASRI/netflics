1e350a9e3d52046c3cc7b0d031745ca9897e981e
LASRi iMAD <imadlasri@hotmail.fr>
2023-12-05 23:15:26 +0100

    fix: filter text color and background

--- src/components/FilterItems.vue
+++ src/components/FilterItems.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { defineProps,defineEmits, ref } from 'vue'
+import { ref } from 'vue'
 
 defineProps({
   categories: {
@@ -18,12 +18,13 @@ const selectedCategory = ref('popular')
   <div class="flex items-center my-5">
     <span class="text-lg lg:text-2xl">Filter by category: </span>
     <select
-      class="text-black text-lg lg:text-xl ml-5 w-40 h-6 lg:w-52 lg:h-10 rounded-md text-center"
+      class="text-lg lg:text-xl ml-5 w-40 h-6 lg:w-52 lg:h-10 rounded-md text-center bg-gray-800 text-white"
       v-model="selectedCategory"
       @change="emit('categoryChanged', selectedCategory)"
     >
       <option value="popular" class="text-gray-400">Popular</option>
       <option
+        class="text-white"
         v-for="category in categories"
         :value="category"
         :key="category"
